1b55829d5a6541460099a515334d3cb6ab3423c7
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-26 17:15:27 +1000

    Update firebase_service.py

--- app/ services/firebase_service.py
+++ app/ services/firebase_service.py
@@ -1,1 +1,10 @@
+# ───────────── app/services/firebase_service.py ─────────────
+import firebase_admin
+from firebase_admin import credentials, firestore
 
+def init_firebase():
+    if not firebase_admin._apps:
+        cred = credentials.Certificate("db_config/firebase_config.json")
+        firebase_admin.initialize_app(cred)
+
+db = firestore.client()
